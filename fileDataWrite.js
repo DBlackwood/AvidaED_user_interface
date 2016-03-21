4ddce03d460a6e3c09346f2e0d628efd10aab31b
Diane Blackwood <Diane.Blackwood@gmail.com>
2016-03-21 14:56:07 -0400

    add PRECALC_PHENOTYPE 1 to avida.cfg

--- fileDataWrite.js
+++ fileDataWrite.js
@@ -72,6 +72,7 @@ av.fwt.makeFzrAvidaCfg = function (idStr, actConfig) {
   txt += 'AVE_TIME_SLICE ' + dijit.byId('aveTimeSlice').get('value') + '\n';
   txt += 'SLEEP_DELAY ' + dijit.byId('sleepDelay').get('value') + '\n';
   txt += '#include instset.cfg\n';
+  txt += 'PRECALC_PHENOTYPE 1';
   if (actConfig) {av.fwt.makeActConfigFile('avida.cfg', txt);}
   else {av.fwt.makeFzrFile(idStr+'/avida.cfg', txt);}
 }
