788e3a5faa7d3911112813c4e684a4706b61d1be
Diane Blackwood <Diane.Blackwood@gmail.com>
2016-05-22 22:16:00 -0400

    got Selected Organsim age to be one row high instead of 2

--- AvidaED.js
+++ AvidaED.js
@@ -624,6 +624,7 @@ require([
     //console.log('popBot ====', document.getElementById('popBot').style.width); //need abotu 430 px for button arrangement to look good.
     //console.log('sot', document.getElementById('sotTopTable').style);
     //console.log('pop', document.getElementById('popsTopTable').style);
+    console.log('nowrap',document.getElementById('tmp').clientWidth);
   });
 
   //Adjust Statistics area width based on gridholder size and shape. gridholder should be roughly square
